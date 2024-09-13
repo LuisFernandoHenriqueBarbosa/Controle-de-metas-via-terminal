8ec78fcbc231084e19f9e62d677eab965b1b32bd
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-09-13 17:14:52 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { select, input, checkbox } = require("@inquirer/prompts");
 let meta = {
   value: "Tomar 3L de água por dia",
   checked: false,
-}
+};
 let metas = [meta];
 
 const cadastrarMeta = async () => {
@@ -15,32 +15,48 @@ const cadastrarMeta = async () => {
   }
 
   metas.push({ value: meta, checked: false });
-}
+};
 
 const listarMetas = async () => {
   const resposta = await checkbox({
     message:
       "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
     choices: [...metas],
-    instructions: false
-  })
-  if (resposta.length == 0){
-     console.log("Nenhuma meta selecionada!")
-     return
+    instructions: false,
+  });
+  if (resposta.length == 0) {
+    console.log("Nenhuma meta selecionada!");
+    return;
   }
 
-  metas.forEach((m) =>{
-     m.checked = false
-  })
+  metas.forEach((m) => {
+    m.checked = false;
+  });
 
   resposta.forEach((resposta) => {
-     const meta = metas.find((m) => {
-          return m.value == resposta
-     })
-     meta.checked = true
-  })
-  console.log('Meta(s) concluídas(s)')
-}
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+    meta.checked = true;
+  });
+  console.log("Meta(s) concluídas(s)");
+};
+
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked;
+  });
+
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas!");
+    return;
+  }
+
+  await select({
+    message: "Metas realizadas",
+    choices: [...realizadas],
+  });
+};
 
 const start = async () => {
   while (true) {
@@ -56,11 +72,15 @@ const start = async () => {
           value: "listar",
         },
         {
+          name: "Metas realizadas",
+          value: "realizadas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
       ],
-    })
+    });
 
     switch (opcao) {
       case "cadastrar":
@@ -70,11 +90,14 @@ const start = async () => {
       case "listar":
         await listarMetas();
         break;
+      case "realizadas":
+        await metasRealizadas();
+        break;
       case "sair":
         console.log("Até a próxima!");
         return;
     }
   }
-}
+};
 
-start()
+start();
